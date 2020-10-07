f5ebba1a24cec6401c2c946d812ca7876ee80cc9
Gerasimov <gev.evg.geradot@gmail.com>
2020-10-07 14:32:49 +0300

    add

---
+++ Breakpoint/Mobs.py
@@ -0,0 +1,23 @@
+import pygame
+
+W = 800
+H = 600
+FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+
+class Mobs(pygame.sprite.Sprite):
+    def __init__(self, color, width, height):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((width, height))
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+        pygame.draw.rect(self.image, color, (0, 0, width, height))
+
+        self.rect = self.image.get_rect()
